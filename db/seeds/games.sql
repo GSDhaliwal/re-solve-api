4e87a8d9c7bc2309925d7799707eea847c2a1220
HenryMao <henry60308@live.cn>
2020-09-09 07:26:38 +0000

    one mistake on games

--- db/seeds/games.sql
+++ db/seeds/games.sql
@@ -4,7 +4,7 @@ VALUES (1, 1, 'bd', false,true ,17);
 
 INSERT INTO games 
 (id, created_quiz_id, game_code, competition_mode_enabled, is_active, host_id)
-VALUES (2, 2, 'ab', false, false, 20);
+VALUES (2, 1, 'ab', false, false, 20);
 
 INSERT INTO games (id, created_quiz_id, game_code, competition_mode_enabled, is_active, host_id)
 VALUES (3, 1, 'abc', false, false, 2);
